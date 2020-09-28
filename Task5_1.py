e8877fc0f4cc3ad23e2109588d11aa90b436b6a7
Dina Volkova <vladina>
2020-09-28 18:57:37 +0300

    Update Task5_1.py

--- Task5_1.py
+++ Task5_1.py
@@ -1,10 +1,24 @@
 a = ''
 a_init = "*"
 i = 0
+word = '*****'
+stop = len(word)
 for i in range(0, 5):
     i += 1
     a += a_init
     print(a)
+for i in word:
+    stop -= 1
+    print(word[:stop])
+
+
+
+
+# word = input()
+# for char in word:
+#     print(char)
+
+
 
 
 
